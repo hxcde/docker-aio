fc6b783f2959c45a8e5057fddc13908c4f91f4b2
hxcde <30338980+hxcde@users.noreply.github.com>
2023-01-19 23:54:41 +0100

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,9 @@
+echo installing Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
+echo Docker installed
