a2979883e4f42f889536a3f57e91b3da0c9e84e3
hxcde <30338980+hxcde@users.noreply.github.com>
2023-01-20 09:56:23 +0100

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,5 +1,5 @@
 echo installing Docker
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose 
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose 
 echo Docker installed
